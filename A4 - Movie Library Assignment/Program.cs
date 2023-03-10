dd7715814f52c185dfdf3d53ac5841f137d345d5
Johnathan Voss <JVoss11@my.wctc.edu>
2023-03-09 21:45:49 -0600

    A5 assignment - Converting to use JSON.

--- A4 - Movie Library Assignment/Program.cs
+++ A4 - Movie Library Assignment/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System.Data;
 using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Linq;
+using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace A4___Movie_Library_Assignment
 {
     internal class Program
     {
         //Need to point to the file.
-        static string movieFile = $"{Environment.CurrentDirectory}/data/movies.csv";
+        static string movieFile = $"{Environment.CurrentDirectory}/data/movies.json";
+        //Point to the Show.csv
+        static string showFile = $"{Environment.CurrentDirectory}/data/shows.csv";
+        //point to the video.csv
+        static string videoFile = $"{Environment.CurrentDirectory}/data/videos.csv";
 
         static void Main(string[] args)
         {
@@ -16,11 +25,11 @@ namespace A4___Movie_Library_Assignment
 
             //1-Create Movie Listing Console Application
 
-            
-            
+
+
 
             //Just want to make sure it exists.
-            if (!File.Exists(movieFile))
+            if (!File.Exists(movieFile) | !File.Exists(showFile) | !File.Exists(videoFile))
             {
                 Console.WriteLine("File is missing!");
                 return;
@@ -29,12 +38,15 @@ namespace A4___Movie_Library_Assignment
                 Console.WriteLine("File is found! Good to continue!\r\n");
             }
 
-            DataTable dt = CSVtoDataTable(movieFile, csvDelimiter);
+            List<Movie> movies = GetMovies(movieFile);
+            DataTable dtV = CSVtoDataTable(videoFile, csvDelimiter);
+            DataTable dtS = CSVtoDataTable(showFile, csvDelimiter);
 
             //Ask for which option the user wants.
             Console.WriteLine("Welcome to the Movie Library!");
             Console.WriteLine("1 - Add a Movie to the Library.");
             Console.WriteLine("2 - Display all movies from the Library.");
+            Console.WriteLine("3 (NEW! - A6) - Display movies or shows or videos!.");
             Console.WriteLine("Any other button to exit.\r\n");
 
 
@@ -43,19 +55,28 @@ namespace A4___Movie_Library_Assignment
             if (userInput == "1")
             {
                 Console.WriteLine("Adding to Library!\r\n");
-
+                var movie = new Movie();
                 //movieID, title, genres
-                int maxID = Convert.ToInt32(dt.Compute("max([movieID])", string.Empty));
+                
                 //Asking user for Movie Title.
                 Console.WriteLine("Please Input Movie Title: ");
-                string titleInput = Console.ReadLine();
+                movie.Title = Console.ReadLine();
                 //Asking user for Movie Genres.
                 Console.WriteLine("Please Input Movie Genres: ");
-                string genresInput = Console.ReadLine();
+                movie.Genres.Add(Console.ReadLine());
 
+                long newID = 1;
+                if (movies.Count > 0)
+                {
+                    newID = movies.MaxBy(x => x.Id).Id;
+                    newID++;
+                }
 
-                dt.Rows.Add(maxID++, titleInput, genresInput);
-                SaveToCSV(dt, ',');
+                movie.Id = newID;
+
+                movies.Add(movie);
+                SaveMovies(movieFile, movies);
+                
 
 
 
@@ -84,7 +105,8 @@ namespace A4___Movie_Library_Assignment
                 Console.WriteLine("Enter a Movie Title Keyword to search");
                 string userSearch = Console.ReadLine();
 
-                DataRow[] result = dt.Select($"title LIKE '%{EscapeLikeValue(userSearch)}%'");
+                /*
+                DataRow[] result = movies.Select($"title LIKE '%{EscapeLikeValue(userSearch)}%'");
                 //Returning number of results found, in case the keyword returns nothing.
                 Console.WriteLine("Results found: {0}", result.Length);
                 foreach (DataRow row in result)
@@ -94,17 +116,113 @@ namespace A4___Movie_Library_Assignment
 
 
                 }
+                */
 
 
 
+            }
+            else if (userInput == "3")
+            {
 
-            } else
+                // ===========================================================
+                // This is where the A6 Assignment should begin
+                // ===========================================================
+
+                /* Look at the assignment closely.
+                 * 1- Create shows and videos.csv files using the provided format 
+                 * 1A- Added through the use of abstract classes
+                 * https://github.com/mcarthey/Class06-20024/blob/master/Context/MediaManager.cs
+                 * 
+                 * 2- Add an abstract Display() method to each class to output the content.
+                 * 
+                 * 3- Ask user which TYPE of media (Movies, shows, videos)
+                 * 
+                 * 4- Display the specific contents of that file.
+                 
+                */
+
+                // ===========================================================
+
+                Console.WriteLine("Thank you for trying out our new show and video libraries!");
+                Console.WriteLine("1 - Display the new Video library!");
+                Console.WriteLine("2 - Display the new Show Library!");
+                Console.WriteLine("Please select an option.");
+                userInput = Console.ReadLine();
+
+                if (userInput == "1")
+                {
+
+                    DataRow[] result = dtV.Select();
+                    //Returning number of results found, in case the keyword returns nothing.
+                    Console.WriteLine("Results found: {0}", result.Length);
+                    foreach (DataRow row in result)
+                    {
+                        // videoID,title,format,length,regions
+                        Console.WriteLine($"{row.Field<string>(0)} - {row.Field<string>(1)} - {row.Field<string>(2)} - {row.Field<string>(3)} - {row.Field<string>(4)}");
+
+                    }
+
+                }
+                else if (userInput == "2")
+                {
+                    DataRow[] result = dtS.Select();
+                    //Returning number of results found, in case the keyword returns nothing.
+                    Console.WriteLine("Results found: {0}", result.Length);
+                    foreach (DataRow row in result)
+                    {
+                        // showId,title,season,episode,writer
+                        Console.WriteLine($"{row.Field<string>(0)} - {row.Field<string>(1)} - {row.Field<string>(2)} - {row.Field<string>(3)} - {row.Field<string>(4)}");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Thank you for trying our new libraries!\r\nExiting Program.");
+                }
+            }
+
+            else
             {
                 Console.WriteLine("Exit Program.\r\nThank you for visiting the Library today!");
             }
 
-            
+
         }
+
+
+
+        // ==========================================================
+
+
+
+        //All the stuff to read / write to the datatables are below.
+
+        public static List<Movie> GetMovies(string strFilePath)
+        {
+            var movies = new List<Movie>();
+            using (StreamReader sr = new StreamReader(strFilePath))
+            {
+                movies = JsonSerializer.Deserialize<List<Movie>>(sr.ReadToEnd());
+            }
+            return movies;
+        }
+
+        public static void SaveMovies(string strFilePath, List<Movie> movies)
+        {
+            //File.WriteAllLines(movieFile, output, Encoding.UTF8);
+            using (StreamWriter sw = new StreamWriter(strFilePath))
+            {
+                var options = new JsonSerializerOptions();
+                options.PropertyNameCaseInsensitive = true;
+                options.WriteIndented = true;
+                
+                sw.Write(JsonSerializer.Serialize(movies, options));
+            }
+        }
+
+
+
+        //static string movieFile = $"{Environment.CurrentDirectory}/data/movies.json";
 
         public static DataTable CSVtoDataTable(string strFilePath, char csvDelimiter)
         {
---
+++ A4 - Movie Library Assignment/movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4___Movie_Library_Assignment
+{
+    public class Movie
+    {
+        //movieId,title,genres
+        
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Genres { get; set; }
+
+        public Movie()
+        {
+            Genres = new List<string>();
+        }
+
+    }
+}
